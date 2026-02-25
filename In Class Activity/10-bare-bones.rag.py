2a8c163a54c39d1b356f31057327dbba240e2f41
astridyuan <astridyuan2@gmail.com>
2026-02-24 23:24:52 -0500

    ICA 10

--- In Class Activity/10-bare-bones.rag.py
+++ In Class Activity/10-bare-bones.rag.py
@@ -6,27 +6,29 @@ from llama_index.core import Settings
 import os
 import streamlit as st
 
-Settings.llm = GoogleGenAI(model= 'gemini-2.5-flash')
-Settings.embed_model = GoogleGenAIEmbedding(model_name= 'models/gemini-embedding-001')
-
 # --- SETUP ---
 load_dotenv()
-if not os.getenv("GEMINI_API_KEY"):
+api_key = os.getenv("GEMINI_API_KEY")
+if not api_key:
     raise ValueError("Missing GEMINI_API_KEY in your .env file")
+
+Settings.llm = GoogleGenAI(model= 'gemini-2.5-flash', api_key=api_key)
+Settings.embed_model = GoogleGenAIEmbedding(model_name= 'models/gemini-embedding-001', api_key= api_key)
 
 # --- CORE LOGIC ---
 def get_query_engine():
-    doc = SimpleDirectoryReader("data").load_data()
+    docs = SimpleDirectoryReader("../data").load_data()
     index = VectorStoreIndex.from_documents(docs)
     return index.as_query_engine(similarity_top_k=5)
 
 
 # --- STREAMLIT UI ---
 st.title("Bare Bones Rag Chatbot")
-prompt = streamlit.chat_input("Ask me a question about Babson student handbook...")
+prompt = st.chat_input("Ask me a question about Babson student handbook...")
 query_engine = get_query_engine()
 
 if prompt:
     st.write(f"User: {prompt}")
     # TO DO get response from LLM
+    response = query_engine.query(prompt)
     st.write(f"Chatbot response: {response}")
