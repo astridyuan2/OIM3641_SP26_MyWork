355b7c138640ff0fc50717fa331bf4236d87c4cb
astridyuan <astridyuan2@gmail.com>
2026-02-26 20:50:06 -0500

    revise ica 10

--- In Class Activity/10-bare-bones.rag.py
+++ In Class Activity/10-bare-bones.rag.py
@@ -3,32 +3,31 @@ from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
 from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
 from llama_index.llms.google_genai import GoogleGenAI
 from llama_index.core import Settings
-import os
 import streamlit as st
 
 # --- SETUP ---
 load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
-if not api_key:
-    raise ValueError("Missing GEMINI_API_KEY in your .env file")
-
+DATA_DIR = 'In Class Activity/data/handbook'
+# rate limits for Gemini changed in 2025, which is causing the wall with
+# gemini-embedding-001
 Settings.llm = GoogleGenAI(model= 'gemini-2.5-flash', api_key=api_key)
 Settings.embed_model = GoogleGenAIEmbedding(model_name= 'models/gemini-embedding-001', api_key= api_key)
 
 # --- CORE LOGIC ---
 def get_query_engine():
-    docs = SimpleDirectoryReader("../data").load_data()
+    docs = SimpleDirectoryReader('In Class Activity/data/handbook').load_data()
     index = VectorStoreIndex.from_documents(docs)
-    return index.as_query_engine(similarity_top_k=5)
+    return index.as_query_engine()
 
 
 # --- STREAMLIT UI ---
-st.title("Bare Bones Rag Chatbot")
-prompt = st.chat_input("Ask me a question about Babson student handbook...")
+st.title("Bare Bones RAG Chatbot")
 query_engine = get_query_engine()
+prompt = st.chat_input("Ask me a question...")
 
 if prompt:
     st.write(f"User: {prompt}")
     # TO DO get response from LLM
     response = query_engine.query(prompt)
+    bot_response = response.response
     st.write(f"Chatbot response: {response}")
