1112e82845945da23a81a67436ecd4bbf894b630
astridyuan <astridyuan2@gmail.com>
2026-03-10 21:09:03 -0400

    ICA14

--- In Class Activity/14-huggingface_sentiment-Analysis.py
+++ In Class Activity/14-huggingface_sentiment-Analysis.py
@@ -1,6 +1,16 @@
 from transformers import pipeline
 
-model = pipeline("text-classification",model="yiyanghkust/finbert-tone")
-text = "The stock market rally continued, suggesting strong long-term growth."
-result = model(text)
-print(result)
+model = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+
+text = "The latest press release details the company's new policy on remote work, including guidelines for team communication and hardware allocation for employees worldwide."
+
+labels = ["Employee Relations", "Financial News", "Product Announcement", "Technical Support", "Sales", "HR Policy", "Legal Compliance"]
+
+result = model(text, labels)
+
+print("Zero-Shot Classification Results")
+print(f"Input Text: {result['sequence']}")
+print("Classification Scores:")
+
+for i, (label, score) in enumerate(zip(result['labels'], result['scores'])):
+    print(f"  {i+1}. {label}: {round(score * 100, 2)}%")
